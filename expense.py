4de01f86985be21cb75d0ae3b9264e4265faa3b2
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-05-19 13:31:27 -0700

    update expense_tracker.py
    adding functions to get user expense, amount, and category.

--- expense.py
+++ expense.py
@@ -1,5 +1,5 @@
 class Expense:
-    def __init__(self, name, category, amount):
+    def __init__(self, name, category, amount) -> None:
         self.name = name
         self.category = category
         self.amount = amount
--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,43 @@
 def main():
+    print(f"Running Expense Tracker")
+    # get use irnput for expense.
+    get_user_expense()
+    # write their expense to a file
+    save_expense_to_file()
+    # read file and summarize expense
+    summarize_expense()
+
+
+def get_user_expense():
+    print("getting user expense")
+    expense_name = input("Enter expense name:")
+    expense_amount = float(input("Enter expense amount:"))
+
+    print(f"You've entered {expense_name}, {expense_amount}")
+
+    expense_categories = [
+        "Food", "Home", "Work", "Fun", "Misc",
+    ]
+    
+    while True:
+        print("Select a category: ")
+        for i, category_name in enumerate(expense_categories):
+            print(f"  {i + 1}.{category_name}")
+
+        value_range = f"[1-{len(expense_categories)}]"
+        selected_index = input("Enter a category numer: ")
+        print(selected_index)
+        break
+
+def save_expense_to_file():
+    print(f"savings user expense")
+      
+
+def summarize_expense():
+    print(f"summarizing user expense")
+      
+
+
     pass
 
 if __name__ == "__main__":
