62e81cc4db9e1ebb9bd4d6de3d9807e926856839
Nayeli De Jesus <nayelidjm142@gmail.com>
2025-06-06 15:25:41 -0700

    connected to mongodb

--- db.py
+++ db.py
@@ -1,10 +1,15 @@
+import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import os
+
 
 load_dotenv()
-
 MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["expense_tracker"]
-users_collection = db["users"]
+
+try:
+    client = MongoClient(MONGO_URI) 
+    client.server_info() 
+    print("Connected to MongoDB!")
+except Exception as e:
+    print("Connection failed:", e)
+
